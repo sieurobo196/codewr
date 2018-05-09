037e927e0d249e2b9d986b17686fdde5759a825b
Sieurobo196 <xuancu2006@gmail.com>
2018-05-09 10:39:59 +0700

    edit

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -18,64 +18,49 @@ class ArticlesController extends AppController {
     }
 
     public function add() {
-        $this->log("start upload file add", 'info');
-        $isPost = $this->request->is('post');
-        $this->log("request is $isPost", 'info');
+//        $this->log("add new", "info");
+        $isPost = $this->request->is("post");
         if ($isPost) {
-            $this->log("upload file ", "info");
-            $title = $this->request->data("Title");
-            $mapUrl = $this->request->data("MapUrl");
-            $description_meta = $this->request->data("MetaDes");
-            $keywords = $this->request->data("MetaKey");
-            $description = $this->request->data('Description');
-            $content_article = $this->request->data('Content');
-//            $file = $this->request->data('File');
-            $type = $this->request->data('Type');
+            $title = $this->request->data("title");
+            $mapUrl = $this->request->data("map_url");
+            $description_meta = $this->request->data("metades");
+            $keywords = $this->request->data("metakey");
+            $content_article = $this->request->data("content");
+            $type = $this->request->data("type");
             $newDate = date("Y-m-d H:i:s");
-//            $imageName = date("Y-m-d") . $file["name"];
-            $this->log($title, "info");
-            $this->log($mapUrl, "info");
-            $this->log($description_meta, "info");
-            $this->log($keywords, "info");
-            $this->log($description, "info");
-            $this->log($content_article, "info");
-            $this->log($type, "info");
-            $this->log($newDate, "info");
-//            $this->log($imageName, "info");
-//            $pathFile = SITE_ROOT . "/uploads/articles" . DS . $imageName;
-//            $isUpload = move_uploaded_file($file["tmp_name"], $pathFile);
-//            $this->log("upload $isUpload", "info");
-//            if ($isUpload) {
-            $articles_table = TableRegistry::get('Articles');
-            $article = $articles_table->newEntity();
-            $article->title = $title;
-            $article->map_url = $mapUrl;
-            $article->meta_des = $description_meta;
-            $article->meta_keys = $keywords;
-            $article->des_article = $description;
-//                $article->image = $imageName;
-            $article->type = $type;
-            $article->content = $content_article;
-            $article->createdDate = $newDate;
-            if ($articles_table->save($article)) {
-                echo "Article add success";
-                $this->setAction('index');
+            if ($title == '' || $mapUrl == '' || $type == '' || $content_article == '') {
+                $this->log("error data empty", "error");
             } else {
-                echo "Article add fail";
+                $articles_table = TableRegistry::get("Articles");
+                $article = $articles_table->newEntity();
+                $article->title = $title;
+                $article->map_url = $mapUrl;
+                $article->meta_des = $description_meta;
+                $article->meta_keys = $keywords;
+                $article->type = $type;
+                $article->content = $content_article;
+                $article->createdDate = $newDate;
+                if ($articles_table->save($article)) {
+                    $this->log("added " . $title, "info");
+                    $this->redirect(
+                            "/view/" . $type . "/" . $mapUrl
+                    );
+                } else {
+                    $this->log("added fail" . $title, "info");
+                }
             }
-//            } else {
-//                echo "upload file fail";
-//            }
         }
     }
 
     public function index() {
-        $listArticles = TableRegistry::get('Articles');
+        $this->log("call home", "info");
+        $listArticles = TableRegistry::get("Articles");
         $query = $listArticles->find()->order(["createdDate" => "DESC"]);
         $listNew = $listArticles->find()->order(["createdDate" => "DESC"])->limit(20);
         $listType = $listArticles->find()->select(["type"])->group("type");
-
+//        $this->log($listType,"info");
         $this->set("results", $query);
+        $this->set("activeMenu", "index");
         $this->set("listType", $listType);
         $this->set("listArticleNew", $listNew);
         $this->set("title", "CodeWR Online Web Example");
@@ -85,11 +70,13 @@ class ArticlesController extends AppController {
     }
 
     public function view($type, $mapUrl) {
-        $this->log("call view $mapUrl", "info");
-        $articles = TableRegistry::get('Articles');
+        $this->log("call view " . $type ."/". $mapUrl, "info");
+        $activeMenu = $type == null ? "index" : $type;
+        $this->set("activeMenu", $activeMenu);
+        $articles = TableRegistry::get("Articles");
         $article = $articles->find()->where(['map_url = "' . $mapUrl . '"'])->first();
         if (empty($article)) {
-            $this->redirect(['controller' => 'Articles', 'action' => 'index']);
+            $this->redirect(["controller" => "Articles", "action" => "index"]);
         } else {
             $listType = $articles->find()->select(["type"])->group("type");
             $listNew = $articles->find()->where(["type" => $type])->order(["createdDate" => "DESC"])->limit(10);
@@ -114,31 +101,30 @@ class ArticlesController extends AppController {
             $view = $article->view;
             $view++;
             $article->view = $view;
+            $newDate = date("Y-m-d H:i:s");
+            $article->updatedDate = $newDate;
             if ($articles->save($article)) {
                 
             } else {
-                $this->log("update view fail" + $view, "info");
+                $this->log("call view fail" . $type . $mapUrl, "error");
             }
         }
     }
 
     public function detail($type) {
-        $this->log("call detail $type", "info");
-        $listArticles = TableRegistry::get('Articles');
-        $listArticle = $listArticles->find()->where(["type" => $type])->order(["createdDate" => "DESC"]);
+        $this->log("call detail " . $type, "info");
+        $listArticles = TableRegistry::get("Articles");
+        $listArticle = $listArticles->find()->where(['type LIKE' => '%'.$type.'%'], ['type' => 'string'])->order(["createdDate" => "DESC"]);
         $listNew = $listArticles->find()->where(["type" => $type])->order(["createdDate" => "DESC"])->limit(10);
-        $listType = $listArticles->find()->select(["type"])->group("type");
-        if(empty($listArticle)){
-            $this->log("empty ","info");
-        }
+        $listType = $listArticles->find()->select(["type"])->where(['type LIKE' => '%'.$type.'%'], ['type' => 'string'])->group("type");
         $count = 0;
         foreach ($listArticle as $article) {
             $count++;
         }
-        $this->log("count " . $count, "info");
+//        $this->log("count " . $count, "info");
         if ($count == 0) {
-
-            $this->redirect(['controller' => 'Articles', 'action' => 'index']);
+            $this->log("no article ", "info");
+            $this->redirect("/");
         } else {
             $this->set("results", $listArticle);
             $this->set("listType", $listType);
@@ -151,43 +137,50 @@ class ArticlesController extends AppController {
     }
 
     public function edit($id) {
-        if ($this->request->is('post')) {
-            $title = $this->request->data("Title");
-            $mapUrl = $this->request->data("MapUrl");
-            $description_meta = $this->request->data("MetaDes");
-            $keywords = $this->request->data("MetaKey");
-            $description = $this->request->data('Description');
-            $content_article = $this->request->data('Content');
-            $type = $this->request->data('Type');
+        $this->log("call edit " . $id, "info");
+        if ($this->request->is("post")) {
+//            $this->log("update article", "info");
+            $title = $this->request->data("title");
+            $mapUrl = $this->request->data("map_url");
+            $description_meta = $this->request->data("metades");
+            $keywords = $this->request->data("metakey");
+            $content_article = $this->request->data("content");
+            $type = $this->request->data("type");
             $newDate = date("Y-m-d H:i:s");
-            $articles = TableRegistry::get('Articles');
-            $article = $articles->get($id);
-            $article->title = $title;
-            $article->map_url = $mapUrl;
-            $article->meta_des = $description_meta;
-            $article->meta_keys = $keywords;
-            $article->des_article = $description;
-            $article->content = $content_article;
-            $article->type = $type;
-            $article->updatedDate = $newDate;
-
-            if ($articles->save($article)) {
-                echo "Article is udpated";
+            
+            if ($title == '' || $mapUrl == '' || $type == '' || $content_article == '') {
+                $this->log("error data empty", "error");
             } else {
-                echo "Article update failed";
+                $articles = TableRegistry::get("Articles");
+                $article = $articles->get($id);
+                $article->title = $title;
+                $article->map_url = $mapUrl;
+                $article->meta_des = $description_meta;
+                $article->meta_keys = $keywords;
+                $article->content = $content_article;
+                $article->type = $type;
+                $article->updatedDate = $newDate;
+
+                if ($articles->save($article)) {
+                    $this->log("udpated " . $mapUrl, "info");
+                } else {
+                    $this->log("updated failed " . $id, "error");
+                }
+                $this->redirect(
+                        "/view/" . $type . "/" . $mapUrl
+                );
             }
-            $this->setAction('index');
         } else {
-            $articles = TableRegistry::get('Articles')->find();
-            $article = $articles->where(['id' => $id])->first();
-            $this->set('title', $article->title);
-            $this->set('mapUrl', $article->map_url);
-            $this->set('meta_des', $article->meta_des);
-            $this->set('meta_keys', $article->meta_keys);
-            $this->set('des_article', $article->des_article);
-            $this->set('content', $article->content);
-            $this->set('type', $article->type);
-            $this->set('id', $id);
+            $articles = TableRegistry::get("Articles")->find();
+            $article = $articles->where(["id" => $id])->first();
+            $this->set("title", $article->title);
+            $this->set("mapUrl", $article->map_url);
+            $this->set("meta_des", $article->meta_des);
+            $this->set("meta_keys", $article->meta_keys);
+            $this->set("des_article", $article->des_article);
+            $this->set("content", $article->content);
+            $this->set("type", $article->type);
+            $this->set("id", $id);
         }
     }
 
